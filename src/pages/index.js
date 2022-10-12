ea261737317daf3d33a975838e5b3881a1f90551
Josh Derocher-Vlk <josh.derocher-vlk@simplisafe.com>
2022-10-12 12:18:56 -0400

    fix: basic site build working

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,41 +5,7 @@ import * as sections from "../components/sections"
 import Fallback from "../components/fallback"
 
 export default function Homepage(props) {
-  const { homepage } = props.data
-
   return (
-    <Layout {...homepage}>
-      {homepage.blocks.map((block) => {
-        const { id, blocktype, ...componentProps } = block
-        const Component = sections[blocktype] || Fallback
-        return <Component key={id} {...componentProps} />
-      })}
-    </Layout>
+      <h1 style={{ textAlign: 'center' }}>Hello world!</h1>
   )
 }
-
-export const query = graphql`
-  {
-    homepage {
-      id
-      title
-      description
-      image {
-        id
-        url
-      }
-      blocks: content {
-        id
-        blocktype
-        ...HomepageHeroContent
-        ...HomepageFeatureListContent
-        ...HomepageCtaContent
-        ...HomepageLogoListContent
-        ...HomepageTestimonialListContent
-        ...HomepageBenefitListContent
-        ...HomepageStatListContent
-        ...HomepageProductListContent
-      }
-    }
-  }
-`
